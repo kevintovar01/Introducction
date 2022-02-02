536ccece7aa97dcb84d635690b92daab4ce65097
kevintovar01 <kevinetl01@gmail.com>
2022-02-02 15:43:54 -0500

    Update

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,23 @@
 #Django
-from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import View, UpdateView
+from django.urls import reverse_lazy
 
 #Forms
 from .forms import PostCreateForm
+
+#models
 from .models import Post
 
 # Create your views here.
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
-        context = {
-
+        posts = Post.objects.all()
+        context = { 
+            'posts':posts
         }
         return render(request, 'blog_list.html', context)
+
 
 class BlogCreateView(View):
     def get(self, request, *args, **kwargs):
@@ -35,4 +40,23 @@ class BlogCreateView(View):
 
         context = {
         }
-        return render(request, 'blog_create.html', context)       
+        return render(request, 'blog_create.html', context)
+
+
+class BlogDetailView(View):
+    def get(self, request, pk, *args, **kwargs):
+        post = get_object_or_404(Post, pk=pk)
+        context = {
+            'post':post,
+        }
+        return render(request, 'blog_detail.html', context)
+
+
+class BlogUpdateView(UpdateView):
+    model=Post
+    fields=['title','content']
+    template_name='blog_update.html'
+    
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('blog:detail', kwargs={'pk':pk})
